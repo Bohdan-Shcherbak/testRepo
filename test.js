d34ece58b7db7572310f47bbf9a97205e93a3a63
Brocoli <Bohdansherbak@gmail.com>
2022-06-17 22:33:55 +0300

    test.js

---
+++ test.js
@@ -0,0 +1,33 @@
+// let i = 0;
+// let c = 0; 
+// const str = 'Rolling Scopes Schl'; 
+// while (i < str.length)
+// { if (str[i] === 'o') 
+// { c++; } i++; }
+// console.log(c);
+
+// let i = 0; 
+// while (i <= 3) 
+// {console.log(i); i++;}
+
+// const name = 'Vasia'; 
+// Math.abs(name.length - 15);
+// abs = result;
+// console.log(Math.min(10, 222, -5, 0, '-33'));
+// a = true;
+// b = true;
+// console.log(!(a || b));
+// const str = 'abcde';
+
+// const last = Message.charAt(Message.length - 1);
+// console.log(last); 
+// a = 100, b = 15;
+// const rfr = Math.max = (a > b ? a : b)
+// console.log(rfr);
+
+// let sum = (a,b) => a + b;
+// let sum = function(a,b) { return a + b };
+
+// 'Vasia'.toUpperCase().length.toString()
+
+'\nHey!'[1]
